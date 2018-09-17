58e4f753a833ea22c0084ab9cea3d8ed38676084
Misael Alanis <misaelarturo@gmail.com>
2018-09-17 01:22:02 -0500

    Agrega datos de prueba

--- usuarios.sql
+++ usuarios.sql
@@ -1,2 +1,14 @@
 INSERT INTO `usuarios`(`usuario`, `passwd`, `email`)
 VALUES('mcsonk', '1234', 'misaelarturo@gmail.com');
+
+INSERT INTO `colores`(`descripcion`)
+VALUES('Rojo'), ('Azul'), ('Verde');
+
+INSERT INTO `materiales`(`descripcion`)
+VALUES('Madera'), ('Oro'), ('Cobre');
+
+INSERT INTO `deportes`(`descripcion`)
+VALUES('Natación'), ('Box'), ('Futbol');
+
+INSERT INTO `categorias`(`descripcion`)
+VALUES('Bajo'), ('Medio'), ('Alto');
