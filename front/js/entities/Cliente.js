8957441944ef97c83f4272d9a66157fb7f23cb34
omarh7 <eomarhoyose@gmail.com>
2019-05-02 23:50:53 -0500

    Agregando frontend y backend para seccion de Clientes y Pedidos

---
+++ front/js/entities/Cliente.js
@@ -0,0 +1,7 @@
+function Cliente(id, nombre, direccion, vendedorInt, method){
+	this.id = id;
+	this.nombre = nombre;
+	this.direccion = direccion;
+	this.vendedorInt = vendedorInt;
+	this.method = method;
+}
---
+++ src/controller/PedidoController.php
@@ -0,0 +1,69 @@
+<?php
+/**
+* 
+*/
+require_once dirname(__DIR__)."/business/PedidoBusiness.php";
+require_once dirname(__DIR__)."/business/SessionBusiness.php";
+require_once dirname(__DIR__)."/utils/Loger.php";
+
+
+$pedidoBusiness = new pedidoBusiness();
+$sessionBusiness = new SessionBusiness();
+$sessionBusiness->checkSession();
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	switch ($_POST['method']) {
+		case 'deleteElement':
+			$pedido = new Pedido($_POST);
+			$pedidoBusiness->deleteElement($pedido);
+			break;
+		case 'createOrUpdateElement':
+			#Loger::log(print_r($_POST, 1), null);
+			$pedido = new Pedido($_POST);
+			$pedidoBusiness->createOrUpdateElement($pedido);
+			break;
+		case 'setMeasure':
+			$pedido = new Pedido($_POST);
+			$medida = new Measure($_POST);
+			$pedidoBusiness->setMeasure($pedido, $medida);
+			break;
+		case 'setMeasures':
+			$pedido = new Pedido($_POST['elementoUpdate']);
+			$medidas = array();
+			foreach($_POST['elementoUpdate']['medidas'] as $medida){
+				array_push($medidas, new Measure($medida["id"], $medida["descripcion"]));
+			}
+			$pedidoBusiness->setMeasures($pedido, $medidas);
+			break;
+		case 'deleteMedidaElemento':
+			$pedido = new Pedido($_POST['elemento']);
+			$medida = new Measure($_POST['medida']);
+			$pedidoBusiness->deleteMedidaElemento($pedido, $medida);
+			break;
+		default:
+
+			break;
+	}
+}
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+	switch ($_GET['method']) {
+		case 'getOrdersGrid':
+			$pedido = null;
+			if(isset($_GET['filters']) && $_GET['filters'] != null){
+				$pedido = new Pedido($_GET['filters']);
+			}
+			$pedidoBusiness->getElementsGrid($pedido);
+			break;
+		case 'getElementosTrofeos':
+			$pedido = null;
+			if(isset($_GET['filters']) && $_GET['filters'] != null){
+				$pedido = new Pedido($_GET['filters']);
+			}
+			$pedidoBusiness->getElementosTrofeos($pedido);
+			break;
+		default:
+			break;
+	}
+}
+
+?>
