7eafdeee3bc9932f21d93c5b3b783f44555a0b8e
McSonk <misaelarturo@gmail.com>
2019-12-31 12:03:10 -0600

    Actualiza las credenciales de acceso a la BD

--- src/utils/ConnectionStrings.php
+++ src/utils/ConnectionStrings.php
@@ -1,6 +1,6 @@
 <?php
-define("DB_USER", "root");
-define("DB_PASSWD", "root");
+define("DB_USER", "trofeos");
+define("DB_PASSWD", "1234");
 define("DB_HOST", "127.0.0.1");
 define("DB_NAME", "trofeoslobo");
 ?>
