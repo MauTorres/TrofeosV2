e1f16c7358bb3324e3e4c206528aa1f81e57ecf9
Misael Alanis <misaelarturo@gmail.com>
2020-01-21 00:53:31 -0600

    Mejora el manejo de errores del objeto catálogo

--- front/js/controller/CatalogCreator.js
+++ front/js/controller/CatalogCreator.js
@@ -3,16 +3,31 @@ function CatalogCreator(catalogURL){
 	this.catalogData
 	this.catalogURL = catalogURL;
 	this.fillCatalog = function(catalog){
-		$.when(httpCall('GET', self.catalogURL, {method: 'getElementsGrid'})).done(function(result){
-			try{
-				self.catalogData = jQuery.parseJSON(result);
-				self.catalogData = self.catalogData.data.resultSet;
-				for(var elementCount = 0; elementCount < self.catalogData.length; elementCount++){
-					var element = self.catalogData[elementCount];
-					catalog.append('<option value="' + element.id + '">' + element.descripcion + '</option>');
+		$.ajax({
+			method: 'GET',
+			url: self.catalogURL,
+			data: {method: 'getElementsGrid'},
+			success: function(result){
+				if( result === null || result === undefined || result.length < 1 ){
+					notifyError("Ocurrió un error en el servidor. Por favor, inténtelo de nuevo");
+					console.warn("No se obtuvo respuesta del servidor");
+					return false;
 				}
-			}catch(err){
-				SessionController.sessionFailed("Ocurrió un error en el servidor");
+				try{
+					self.catalogData = jQuery.parseJSON(result);
+					self.catalogData = self.catalogData.data.resultSet;
+					for(var elementCount = 0; elementCount < self.catalogData.length; elementCount++){
+						var element = self.catalogData[elementCount];
+						catalog.append('<option value="' + element.id + '">' + element.descripcion + '</option>');
+					}
+				}catch(err){
+					console.error(err);
+					notifyError("Ha habido un error inesperado");
+				}
+			},
+			error: function(xhr, textStatus, errorThrown){
+				notifyError("Ha habido un error desconocido");
+				console.error(textStatus + ": " + errorThrown);
 			}
 		});
 	};
