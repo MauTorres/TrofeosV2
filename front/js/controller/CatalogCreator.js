59ed37226685dfdc559295c283b7a61dd82c2222
Misael Alanis <misaelarturo@gmail.com>
2020-01-21 23:13:23 -0600

    Agrega método para cargar catálogo selectivamente

--- front/js/controller/CatalogCreator.js
+++ front/js/controller/CatalogCreator.js
@@ -2,28 +2,43 @@ function CatalogCreator(catalogURL){
 	var self = this;
 	this.catalogData
 	this.catalogURL = catalogURL;
-	this.fillCatalog = function(catalog){
+	var _alreadyFilled = false;
+
+	var _fillGrid = function(result, catalog, context){
+		if( result === null || result === undefined || result.length < 1 ){
+			notifyError("Ocurrió un error en el servidor. Por favor, inténtelo de nuevo");
+			console.warn("No se obtuvo respuesta del servidor");
+			return false;
+		}
+		try{
+			context.catalogData = jQuery.parseJSON(result);
+			context.catalogData = context.catalogData.data.resultSet;
+			for(var elementCount = 0; elementCount < context.catalogData.length; elementCount++){
+				var element = context.catalogData[elementCount];
+				catalog.append('<option value="' + element.id + '">' + element.descripcion + '</option>');
+			}
+		}catch(err){
+			console.error(err);
+			notifyError("Ha habido un error inesperado");
+			return false;
+		}
+		return true;
+	};
+
+	var _fillIfNeeded = function(catalog){
+		if(!_alreadyFilled){
+			_fillCatalog(catalog);
+		}
+	};
+
+
+	var _fillCatalog = function(catalog){
 		$.ajax({
 			method: 'GET',
 			url: self.catalogURL,
 			data: {method: 'getElementsGrid'},
 			success: function(result){
-				if( result === null || result === undefined || result.length < 1 ){
-					notifyError("Ocurrió un error en el servidor. Por favor, inténtelo de nuevo");
-					console.warn("No se obtuvo respuesta del servidor");
-					return false;
-				}
-				try{
-					self.catalogData = jQuery.parseJSON(result);
-					self.catalogData = self.catalogData.data.resultSet;
-					for(var elementCount = 0; elementCount < self.catalogData.length; elementCount++){
-						var element = self.catalogData[elementCount];
-						catalog.append('<option value="' + element.id + '">' + element.descripcion + '</option>');
-					}
-				}catch(err){
-					console.error(err);
-					notifyError("Ha habido un error inesperado");
-				}
+				_alreadyFilled = _fillGrid(result, catalog, self);
 			},
 			error: function(xhr, textStatus, errorThrown){
 				notifyError("Ha habido un error desconocido");
@@ -31,4 +46,7 @@ function CatalogCreator(catalogURL){
 			}
 		});
 	};
+
+	this.fillCatalog = _fillCatalog;
+	this.fillIfNeeded = _fillIfNeeded;
 }
