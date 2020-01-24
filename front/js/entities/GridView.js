35d41500a2f145a0f5eb3e550cfdbfb20601e4a4
Misael Alanis <misaelarturo@gmail.com>
2020-01-23 22:56:24 -0600

    Reestructura el método de getGrid

--- front/js/entities/GridView.js
+++ front/js/entities/GridView.js
@@ -2,23 +2,29 @@ function GridView(){
 	this.elements = null;
 	this.headers = null;
 	var self = this;
-	this.getGrid = function(data, rootURL, actions, table, elementsToDisplay){
-		$.when(httpCall('GET', rootURL, data)).done(function(res){
-			try{
-				var responce = jQuery.parseJSON(res);
-				responce.data.actions = actions;
-				if(responce.success){
-					TableCreator.fillTable(responce.data, table, elementsToDisplay);
-					self.elements = responce.data.resultSet;
+
+	var _getGrid = function(data, rootURL, actions, table, elementsToDisplay){
+		$.ajax({
+			method: 'get',
+			url: rootURL,
+			data: data,
+			dataType: 'json',
+			success: function(response){
+				response.data.actions = actions;
+				if(response.success){
+					TableCreator.fillTable(response.data, table, elementsToDisplay);
+					self.elements = response.data.resultSet;
 					self.elements.forEach(function(element){
 						element.selected = false;
 					});
 				}
-			}catch(err){
-				console.error(err);
-				alert("Ha ocurrido un error en el servidor\n" + err);
-				return;
+			},
+			error: function(xhr, textStatus, errorThrown){
+				notifyError("Ha habido un error desconocido");
+				console.error(textStatus + ": " + errorThrown);
 			}
 		});
-	};
+	}
+
+	this.getGrid = _getGrid;
 }
