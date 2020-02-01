8b48a9b26181aea8840cb6d006cc45240149e6d3
MauTorres <matrescom@hotmail.com>
2020-01-31 20:23:20 -0600

    To master

--- src/utils/ConnectionStrings.php
+++ src/utils/ConnectionStrings.php
@@ -1,7 +1,6 @@
 <?php
-define("DB_USER", "trofeos");
-//define("DB_PASSWD", "1234");
-define("DB_PASSWD", "n0m3l0s3");
+define("DB_USER", "root");
+define("DB_PASSWD", "root");
 define("DB_HOST", "127.0.0.1");
 define("DB_NAME", "trofeoslobo");
 ?>
